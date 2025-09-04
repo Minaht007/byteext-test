1905f96c21481d8a17a3bcb390e93425786698be
Andrey <108063187+Minaht007@users.noreply.github.com>
2025-09-04 12:55:31 +0300

    start

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <p>CONSCIOUSLY MADE BUTTER SOFT STAPLES FOR EVERY DAY (OR NIGHT) | FREE SHIPPING on orders &gt; $200 | easy 45 day return window.</p>
+        </div>
+    )
+}
+
+export default Header
