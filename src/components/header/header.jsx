14468a59ae6755ea9a3272a62a842aed722c2a97
Andrey <108063187+Minaht007@users.noreply.github.com>
2025-09-04 13:42:40 +0300

    header ready

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,32 @@
-
+"use client"
+import { useEffect, useState } from "react"
+import styles from "./header.module.scss"
 
 const Header = () => {
+
+    const phraseOne = "CONSCIOUSLY MADE BUTTER SOFT STAPLES FOR EVERY DAY (OR NIGHT) | FREE SHIPPING on orders &gt; $200 | easy 45 day return window."
+        const phraseTwo = "FREE SHIPPING on orders > $200"
+
+    const [text, setText] = useState(phraseTwo)
+
+    useEffect(() => {
+        const handleResize = () => {
+          if (window.innerWidth < 1200) {
+            setText(phraseTwo)
+          } else {
+            setText(phraseOne)
+          }
+        }    
+        handleResize()
+        
+        window.addEventListener("resize", handleResize)
+        return () => window.removeEventListener("resize", handleResize)
+      }, [])
+
+
     return (
-        <div>
-            <p>CONSCIOUSLY MADE BUTTER SOFT STAPLES FOR EVERY DAY (OR NIGHT) | FREE SHIPPING on orders &gt; $200 | easy 45 day return window.</p>
+        <div className={styles.container}>
+            <p className={styles.title}>{text}</p>
         </div>
     )
 }
