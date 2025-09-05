18297825e3adcf872ac30fcbad1f3e37e913d9e3
Andrey <108063187+Minaht007@users.noreply.github.com>
2025-09-05 22:08:42 +0300

    top swiper

--- src/components/top/tioSwiper.jsx
+++ src/components/top/tioSwiper.jsx
@@ -1,62 +1,47 @@
-"use client"
-import React, { useEffect, useState, useRef } from "react"
-import { Swiper, SwiperSlide } from "swiper/react"
-import "swiper/css"
-import "swiper/css/pagination"
-// import "swiper/css/navigation"
-import { Autoplay, Pagination, Navigation } from "swiper/modules"
-import Image from "next/image"
-import styles from "./top.module.scss"
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay } from "swiper/modules";
+import Image from "next/image";
+import styles from "./top.module.scss";
+import slides from "../../../public/json/topSlider.json";
 
 export default function App() {
-  const [sliders, setSliders] = useState([])
-  const progressCircle = useRef(null)
-  const progressContent = useRef(null)
-
-  useEffect(() => {
-    fetch("/json/topSlider.json")
-      .then(res => {
-        if (!res.ok) throw new Error("Failed to fetch JSON")
-        return res.json()
-      })
-      .then(data => setSliders(data))
-      .catch(err => console.error(err))
-  }, [])
-
-  const onAutoplayTimeLeft = (s, time, progress) => {
-    if (progressCircle.current) {
-      progressCircle.current.style.setProperty("--progress", 1 - progress)
-    }
-    if (progressContent.current) {
-      progressContent.current.textContent = `${Math.ceil(time / 1000)}s`
-    }
-  }
-
-  return (
-    <Swiper
-      spaceBetween={30}
-      centeredSlides={true}
-      autoplay={{ delay: 2500, disableOnInteraction: false }}
-      pagination={{ clickable: true }}
-      navigation={true}
-      modules={[Autoplay, Pagination]}
-      onAutoplayTimeLeft={onAutoplayTimeLeft}
-      className="mySwiper"
-      breakpoints={{
-        1200: {
-            slidesPerView: 5, 
-          },
-        390: {
-          slidesPerView: 3, 
-        }  
-      }}
-    >
-      {sliders.map((item) => (
-        <SwiperSlide key={item.id} className={styles.imgContainer} >
-          <Image src={item.img} alt={`slide ${item.id}`} width={270} height={50}  />
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  )
+	return (
+		<Swiper
+			spaceBetween={20}
+			centeredSlides={true}
+			navigation={false}
+			modules={[Autoplay]}
+			loop={true}
+			speed={600}
+			autoplay={{
+				delay: 2500,
+				disableOnInteraction: false,
+			}}
+			slidesPerView={5}
+			className={styles.mySwiper}
+			breakpoints={{
+				390: {
+					slidesPerView: 3,
+				},
+				1200: {
+					slidesPerView: 5,
+				},
+			}}
+		>
+			{slides.map((item) => (
+				<SwiperSlide key={item.id} className={styles.imgContainer}>
+					<Image
+						src={item.img}
+						alt={`slide ${item.id}`}
+						className={styles.sliderImg}
+						width={270}
+						height={50}
+					/>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
 }
-
